631e6a658a525dd2583f2b38d4312d52726b03dc
Alex Hadjiev <36988416+daBawse167@users.noreply.github.com>
2023-11-09 20:40:50 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,35 @@
+from base64 import encodebytes
+from PIL import Image
+
+import io
 import os
-from flask import Flask, render_template, request, session, redirect
+import numpy as np
+import plotly as px
+import pandas as pd
+import matplotlib.pyplot as plt
 
 app = Flask(__name__)
+
 app.config['SECRET_KEY'] = os.urandom(64)
 app.config['SESSION_TYPE'] = 'filesystem'
 app.config['SESSION_FILE_DIR'] = './.flask_session/'
 
-@app.route("/")
-def home():
-    return render_template("home.html")
+@app.route("/response", methods=["GET", "POST"])
+def home(country="China", feature="population"):
+    df = pd.read_csv("World Energy Consumption.csv")
+    country_data = df[df["country"] == country]
 
-@app.route("/response", methods=["GET"])
-def response():
-    return ["hello"]
+    #plt.plot(country_data["year"], country_data[feature])
+    #plt.savefig("graph.png")
+
+    pil_img = Image.open("graph.png", mode="r")
+    byte_arr = io.BytesIO()
+    pil_img.save(byte_arr, format="PNG")
+    encoded_image = encodebytes(byte_arr.getvalue()).decode("ascii")
+
+    response = { 'Status' : 'Success', 'message': 'message', 'ImageBytes': encoded_image}
+    print(response)
+    return jsonify(response)
 
 if __name__=="__main__":
     app.run(debug=True)
