eff34dbe8e0f1b5160d52520194793ef6819c648
Alex Hadjiev <36988416+daBawse167@users.noreply.github.com>
2023-11-12 18:30:24 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, send_file
+from flask import Flask, jsonify, send_file, request
 from base64 import encodebytes
 from PIL import Image
 
@@ -15,8 +15,11 @@ app.config['SECRET_KEY'] = os.urandom(64)
 app.config['SESSION_TYPE'] = 'filesystem'
 app.config['SESSION_FILE_DIR'] = './.flask_session/'
 
-@app.route("/response/<string:country>", methods=["GET", "POST"])
-def home(country, feature="population"):
+@app.route("/response", methods=["GET", "POST"])
+def home(country, feature):
+    country = request.args.get("country", None)
+    feature = request.args.get("feature", None)
+    
     df = pd.read_csv("World Energy Consumption.csv")
     country_data = df[df["country"] == country]
     
